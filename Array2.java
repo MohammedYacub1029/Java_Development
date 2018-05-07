3c1e48bdde982b0839b2ed1ebfa785370087a700
navas <navarifaya@gmail.com>
2018-05-07 17:37:04 +0530

    array programs

---
+++ Array2.java
@@ -0,0 +1,24 @@
+package io.navas.examples;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		int[] a=new int[10];
+		a[0]=10;
+		a[1]=15;
+		a[2]=14;
+		a[3]=16;
+		a[4]=19;
+		a[5]=20;
+		a[6]=29;
+		a[7]=24;
+		a[8]=26;
+		a[9]=25;
+		int temp=a[1];
+		for(int i=1;i<a.length;i++) {
+			
+		}
+
+	}
+
+}
