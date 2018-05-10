4ebb2bc0c4ac6119fedbc014e53f411e6e6bd911
MohammedYacub1029 <39052590+MohammedYacub1029@users.noreply.github.com>
2018-05-10 07:57:52 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,52 @@
+package io.navas.examples;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		
+		String name="x";
+		
+		
+		switch(name) {
+		
+		
+		case "muthu":
+ 
+			System.out.println("Muthu");
+			break;
+			
+		case "yacub":
+			System.out.println("Muthu");
+
+			
+			break;
+		
+		case "mohammed":
+			System.out.println("Muthu");
+
+		
+		break;
+		case "navas" :
+			
+			System.out.println("navas");
+
+			
+			break;
+			
+			
+			
+	     default:
+	    	 
+	    	 System.out.println("name not found!!!!");
+	    	 
+	    	 break;
+				
+		
+		}
+	
+	
+	}
+
+}
